97a638a199dfc0ff5ff211b9e15c4b95d86ad40b
Chronos <1769712655@qq.com>
2021-10-08 23:36:21 +0800

    fix: import path

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -191,7 +191,7 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop, Watch } from "vue-property-decorator";
 import ImgSelector from "./ImgSelector.vue";
-import { GetInitials } from "@/utils";
+import { GetInitials } from "../utils";
 
 @Component({
   components: {
